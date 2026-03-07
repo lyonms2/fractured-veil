84f1b906503b1844b3e4d16b56be264633c5cd9c
Leonardo <54992907+lyonms2@users.noreply.github.com>
2026-03-07 10:54:12 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -67,7 +67,7 @@ function applyGameState(data) {
   eggLayCooldown= data.eggLayCooldown ?? 0;
   poopCount     = data.poopCount    ?? 0;
   dirtyLevel    = data.dirtyLevel   ?? 0;
-  poopCooldown  = data.poopCooldown ?? 180;
+  poopCooldown  = data.poopCooldown ?? 0;
   if(data.vitals) Object.assign(vitals, data.vitals);
   if(data.gs)     Object.assign(gs, data.gs);
   if(data.eggs)   eggsInInventory = data.eggs;
